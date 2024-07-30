ecf865d737661c84432a50f63bc5dfd9896fb95e
Max Tang <maxtang2001@gmail.com>
2024-07-30 16:46:45 -0400

    pdfjs fix #2

--- frontend/src/app/resume/page.jsx
+++ frontend/src/app/resume/page.jsx
@@ -10,7 +10,11 @@ import Logo from "@/components/common/logo";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+// pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+//     'pdfjs-dist/legacy/build/pdf.worker.min.mjs',
+//     import.meta.url,
+// ).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/legacy/build/pdf.worker.min.mjs`;
 
 export default function ResumePage() {
     const resumeLink = "Max_Tang_Resume.pdf";
